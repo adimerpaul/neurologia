c2fd7fb90418aa8827ce37fcbf95499ded474d90
adimerpaul <adimer101@gmail.com>
2023-08-05 08:13:42 -0400

    configurado para nombre y zona horario

--- back/app/Http/Controllers/VideoController.php
+++ back/app/Http/Controllers/VideoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserVideo;
 use App\Models\Video;
 use App\Http\Requests\StoreVideoRequest;
 use App\Http\Requests\UpdateVideoRequest;
@@ -12,5 +13,15 @@ class VideoController extends Controller{
         $date = $request->get('date');
         $videos = Video::whereDate('date', $date)->get();
         return response()->json($videos);
+    }
+    public function verVideo(Request $request){
+        $user_id = $request->user()->id;
+        $video = Video::find($request->id);
+        UserVideo::create([
+            'user_id' => $user_id,
+            'video_id' => $video->id,
+            'date' => date('Y-m-d'), // date('Y-m-d H:i:s'
+            'time' => date('H:i:s')
+        ]);
     }
 }
--- front/src/pages/Dia13Page.vue
+++ front/src/pages/Dia13Page.vue
@@ -5,22 +5,22 @@
         {{ $filters.dateDmY(date) }}
         <q-btn flat round dense icon="o_refresh" @click="videosGet" :loading="loading" />
       </q-timeline-entry>
-      <q-timeline-entry v-for="(item,i) in videos" :key="i.title"
-        :subtitle="item.subtitle"
-        :title="item.title"
-        :avatar="item.avatar?item.avatar:undefined"
+      <q-timeline-entry v-for="(video,i) in videos" :key="i.title"
+        :subtitle="video.subtitle"
+        :title="video.title"
+        :avatar="video.avatar?video.avatar:undefined"
       >
         <div>
-          <div v-html="item.content"></div>
+          <div v-html="video.content"></div>
           <div>
-            <template v-if="item.button">
+            <template v-if="video.button">
               <q-btn size="12px" outline label="Ingresa a la sala" color="blue" class="" no-caps
                      icon-right="o_videocam" type="a" href="https://us05web.zoom.us/j/87909555789?pwd=jnX91HcvJIIccojTMLCatjMV9ZpWAY.1"
-                     target="_blank" v-if="item.urlZoom"
+                     target="_blank" v-if="video.urlZoom"
               />
               <q-btn size="12px" label="Ver video" color="red" class="q-ml-xs" no-caps
                      icon-right="fa-brands fa-youtube" type="a" href="https://www.youtube.com/watch?v=9Q4XZQZVq6c"
-                     target="_blank" v-if="item.urlYoutube"
+                     target="_blank" v-if="video.urlYoutube" @click="verVideo(video)"
               />
             </template>
           </div>
@@ -47,6 +47,9 @@ export default {
     this.videosGet()
   },
   methods: {
+    verVideo (video) {
+      console.log(video)
+    },
     videosGet () {
       this.videos = []
       this.loading = true
