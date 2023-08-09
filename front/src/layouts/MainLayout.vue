7456647b950d06c6aa4b2adbc619219ee17fcd33
adimerpaul <adimer101@gmail.com>
2023-08-09 07:23:51 -0400

    cambiado icono y colocado pwa

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -1,35 +1,92 @@
 <template>
   <q-layout view="lHh Lpr fff">
-    <q-header elevated>
-      <div class="bg-teal-9">
-        <q-btn flat dense round size="10px" icon="fa-brands fa-facebook" color="white" />
-        <q-btn flat dense round size="10px" icon="fa-brands fa-youtube" color="white" />
-      </div>
-      <q-toolbar>
-<!--        <q-btn-->
-<!--          flat-->
-<!--          dense-->
-<!--          round-->
-<!--          icon="menu"-->
-<!--          aria-label="Menu"-->
-<!--          @click="toggleLeftDrawer"-->
-<!--        />-->
-
-        <q-toolbar-title>
-          <div class="text-subtitle2" style="line-height: 1em">
-            4ta Jornada Nacional de
-            Neurología
-            <br>
-            2do Simposio Internacional de
-            Neurología
+    <q-carousel
+      :height="$q.screen.lt.md ? '35vh' : '70vh'"
+      swipeable
+      animated
+      v-model="slide"
+      thumbnails
+      arrows
+      navigation-icon="radio_button_unchecked"
+      infinite
+      autoplay
+      dark
+      control-color="primary"
+    >
+      <!--          <q-carousel-slide :name="1" img-src="WhatsApp Image 2023-08-05 at 15.53.45.jpeg">-->
+      <!--            sdsds-->
+      <!--          </q-carousel-slide>-->
+      <q-carousel-slide :name="1" class="column no-wrap flex-center" img-src="WhatsApp Image 2023-08-05 at 15.53.45.jpeg">
+<!--        <q-icon name="style" size="56px" />-->
+        <div class="text-center full-width" style="background: rgba(0,0,0,.3); color: white; padding: 10px">
+          <div :class="`${$q.screen.lt.md ? 'text-h6' : 'text-h3'} text-left`" style="width: 350px">4ta JORNADA NACIONAL DE NEUROLOGIA</div>
+          “ACTUALIZACION EN ENFERMEDADES NEUROLOGICAS ORURO 2023”
+          <div>
+            <q-btn label="Ingresa aquí" color="primary" class="q-mt-md" no-caps to="/login" />
           </div>
-        </q-toolbar-title>
-
-        <div>
-          <q-btn label="Ingresar" color="red" to="/login" no-caps size="14px" icon="login" />
         </div>
-      </q-toolbar>
-    </q-header>
+      </q-carousel-slide>
+      <q-carousel-slide :name="2" class="column no-wrap flex-center" img-src="WhatsApp Image 2023-08-05 at 15.53.45 (1).jpeg" >
+        <div class="text-center full-width" style="background: rgba(0,0,0,.3); color: white; padding: 10px">
+          <div :class="`${$q.screen.lt.md ? 'text-h6' : 'text-h3'} text-left`" style="width: 350px">2do SIMPOSIO INTERNACIONAL DE NEUROLOGIA</div>
+          “TEMA: ACTUALIZACION EN ENFERMEDADES NEUROLOGICAS”
+          <div>
+            <q-btn label="Ingresa aquí" color="primary" class="q-mt-md" no-caps to="/login" />
+          </div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide :name="3" class="column no-wrap flex-center" img-src="WhatsApp Image 2023-08-05 at 15.53.45 (2).jpeg" >
+        <div class="text-center full-width" style="background: rgba(0,0,0,.3); color: white; padding: 10px">
+          <div :class="`${$q.screen.lt.md ? 'text-h6' : 'text-h3'} text-left`" style="width: 350px">“EMERGENCIAS NEUROLOGICAS” </div>
+          “FECHAS 13 - 14 - 15 - 16 y 17 DE SEPTIEMBRE DE 2023”
+          <div>
+            <q-btn label="Ingresa aquí" color="primary" class="q-mt-md" no-caps to="/login" />
+          </div>
+        </div>
+      </q-carousel-slide>
+      <template v-slot:control>
+        <q-carousel-control
+          position="top-right"
+          :offset="[18, 18]"
+          class="text-white rounded-borders"
+        >
+          <div>
+            <q-btn label="Ingresar" color="red" to="/login" no-caps size="14px" icon="o_person" dense />
+          </div>
+<!--          <q-toggle dense dark color="orange" v-model="autoplay" label="Auto Play" />-->
+        </q-carousel-control>
+      </template>
+    </q-carousel>
+<!--    <q-header elevated>-->
+<!--      <div class="bg-teal-9">-->
+<!--        <q-btn flat dense round size="10px" icon="fa-brands fa-facebook" color="white" />-->
+<!--        <q-btn flat dense round size="10px" icon="fa-brands fa-youtube" color="white" />-->
+<!--      </div>-->
+<!--      <q-toolbar>-->
+<!--&lt;!&ndash;        <q-btn&ndash;&gt;-->
+<!--&lt;!&ndash;          flat&ndash;&gt;-->
+<!--&lt;!&ndash;          dense&ndash;&gt;-->
+<!--&lt;!&ndash;          round&ndash;&gt;-->
+<!--&lt;!&ndash;          icon="menu"&ndash;&gt;-->
+<!--&lt;!&ndash;          aria-label="Menu"&ndash;&gt;-->
+<!--&lt;!&ndash;          @click="toggleLeftDrawer"&ndash;&gt;-->
+<!--&lt;!&ndash;        />&ndash;&gt;-->
+
+<!--        <q-toolbar-title>-->
+<!--          <div class="text-subtitle2" style="line-height: 1em">-->
+<!--            4ta Jornada Nacional de-->
+<!--            Neurología-->
+<!--            <br>-->
+<!--            2do Simposio Internacional de-->
+<!--            Neurología-->
+<!--          </div>-->
+<!--        </q-toolbar-title>-->
+
+<!--        <div>-->
+<!--          <q-btn label="Ingresar" color="red" to="/login" no-caps size="14px" icon="login" />-->
+<!--        </div>-->
+<!--      </q-toolbar>-->
+<!--    </q-header>-->
 
 <!--    <q-drawer-->
 <!--      v-model="leftDrawerOpen"-->
@@ -114,6 +171,7 @@ export default {
   components: { FooterComponent },
   data () {
     return {
+      slide: 1
       // leftDrawerOpen: ref(false),
       // essentialLinks: linksList
     }
--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -1,47 +1,67 @@
 <template>
   <q-page>
-    <q-carousel
-      animated
-      v-model="slide"
-      arrows
-      :height="$q.screen.lt.md ? '35vh' : '60vh'"
-      navigation
-      navigation-icon="radio_button_unchecked"
-      autoplay
-      infinite
-    >
-      <q-carousel-slide name="first" img-src="6I7N22TXFVHAVPUAFRWA35DWCI.avif">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h2">
-            4ta JORNADANACIONAL DE
-            NEUROLOGIA “EL PODER DE TUS
-            BRAZOS ORURO”
-          </div>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="second" img-src="Bucle OWKIN-G.jpg.webp">
-        <div class="absolute-bottom custom-caption">
-<!--          <div class="text-h2">Second stop</div>-->
-          <div class="text-h2">“ACTUALIZACIONES EN
-            ENFERMEDADES NEUROLOGICAS”
-          </div>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="third" img-src="NOTICIA_OCT28_OP2-_FINUT-02-1024x536.webp">
-        <div class="absolute-bottom custom-caption">
-<!--          <div class="text-h2">Third stop</div>-->
-          <div class="text-h2">2do SIMPOSIO INTERNACIONAL DE
-            NEUROLOGIA
-          </div>
-        </div>
-      </q-carousel-slide>
-    </q-carousel>
+<!--    <q-carousel-->
+<!--      animated-->
+<!--      v-model="slide"-->
+<!--      arrows-->
+<!--      :height="$q.screen.lt.md ? '35vh' : '60vh'"-->
+<!--      navigation-->
+<!--      navigation-icon="radio_button_unchecked"-->
+<!--      autoplay-->
+<!--      infinite-->
+<!--    >-->
+<!--      <q-carousel-slide name="first" img-src="6I7N22TXFVHAVPUAFRWA35DWCI.avif">-->
+<!--        <div class="absolute-bottom custom-caption">-->
+<!--          <div class="text-h2">-->
+<!--            4ta JORNADANACIONAL DE-->
+<!--            NEUROLOGIA “EL PODER DE TUS-->
+<!--            BRAZOS ORURO”-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </q-carousel-slide>-->
+<!--      <q-carousel-slide name="second" img-src="Bucle OWKIN-G.jpg.webp">-->
+<!--        <div class="absolute-bottom custom-caption">-->
+<!--&lt;!&ndash;          <div class="text-h2">Second stop</div>&ndash;&gt;-->
+<!--          <div class="text-h2">“ACTUALIZACIONES EN-->
+<!--            ENFERMEDADES NEUROLOGICAS”-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </q-carousel-slide>-->
+<!--      <q-carousel-slide name="third" img-src="NOTICIA_OCT28_OP2-_FINUT-02-1024x536.webp">-->
+<!--        <div class="absolute-bottom custom-caption">-->
+<!--&lt;!&ndash;          <div class="text-h2">Third stop</div>&ndash;&gt;-->
+<!--          <div class="text-h2">2do SIMPOSIO INTERNACIONAL DE-->
+<!--            NEUROLOGIA-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </q-carousel-slide>-->
+<!--    </q-carousel>-->
     <div class="row">
-      <div class="col-2"></div>
-      <div class="col-8">
+<!--      <div class="col-12">-->
+<!--      </div>-->
+      <div class="col-2 col-md-1"></div>
+      <div class="col-8 col-md-4">
+        <div class="text-subtitle2 text-bold text-center">
+          4ta JORNADA NACIONAL DE NEUROLOGIA
+        </div>
+        <div class="text-subtitle2 text-bold text-center">
+          “EL PODER DE TUS BRAZOS ORURO”
+        </div>
         <q-img src="356879877_584668113818175_8097994551976256420_n.jpg" />
       </div>
-      <div class="col-2"></div>
+      <div class="col-2 col-md-1"></div>
+
+      <div class="col-2 col-md-1"></div>
+      <div class="col-8 col-md-4">
+        <div class="text-subtitle2 text-bold text-center">
+          2do SIMPOSIO INTERNACIONAL DE NEUROLOGIA
+        </div>
+        <div class="text-subtitle2 text-bold text-center">
+          “ACTUALIZACIONES EN ENFERMEDADES NEUROLOGICAS”
+        </div>
+        <q-img src="363427687_603117188639934_2648311829907974764_n.jpg" />
+      </div>
+      <div class="col-2 col-md-1"></div>
     </div>
   </q-page>
 </template>
@@ -53,7 +73,7 @@ export default defineComponent({
   name: 'IndexPage',
   data () {
     return {
-      slide: 'first'
+      slide: 1
     }
   }
 })
