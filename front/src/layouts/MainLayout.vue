b603435fe44182d7e525678bc4ae501f2c34ad34
adimerpaul <adimer101@gmail.com>
2023-08-22 07:27:39 -0400

    Update MainLayout.vue

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -10,46 +10,22 @@
       infinite
       autoplay
       dark
-      control-color="primary"
     >
-      <!--          <q-carousel-slide :name="1" img-src="WhatsApp Image 2023-08-05 at 15.53.45.jpeg">-->
-      <!--            sdsds-->
-      <!--          </q-carousel-slide>-->
-      <q-carousel-slide :name="1" class="column no-wrap flex-center" img-src="WhatsApp Image 2023-08-05 at 15.53.45.jpeg">
-<!--        <q-icon name="style" size="56px" />-->
-        <div class="text-center full-width q-pa-lg" style="background: rgba(0,0,0,.3)">
-          <div :class="`${$q.screen.lt.md ? 'text-h6' : 'text-h3'} text-left`" style="width: 100%;padding: 10px">
-            4ta. JORNADA NACIONAL DE NEUROLOGÍA
+      <q-carousel-slide v-for="(t, i) in textos" :name="i+1" :key="i" class="column no-wrap flex-center q-pa-none" :img-src="t.img">
+        <div class="text-center full-width q-pa-lg background">
+          <div class="text-left titulo">
+            {{ t.titulo }}
           </div>
-          Actualización en Enfermedades Neurológicas <br>
-          Fechas: 13, 14, 15 de septiembre de 2023
+          <div class="subtitulo">
+            {{ t.subtitulo }}
+          </div>
+          <br>
+            {{ t.fecha }}
           <div>
             <q-btn label="Ingresa aquí" color="primary" class="q-mt-md" no-caps to="/login" />
           </div>
         </div>
       </q-carousel-slide>
-      <q-carousel-slide :name="2" class="column no-wrap flex-center" img-src="WhatsApp Image 2023-08-05 at 15.53.45 (1).jpeg" >
-        <div class="text-center full-width full-height flex flex-center" style="background: rgba(0,0,0,.3); color: white; padding: 30px">
-          <div :class="`${$q.screen.lt.md ? 'text-h6' : 'text-h3'} text-left`" style="width: 100%;padding: 10px">
-            2DO. SIMPOSIO INTERNACIONAL
-          </div>
-          Emergencias Neurológicas <br>
-          Fechas: 16 y 12 de septiembre
-          <div>
-            <q-btn label="Ingresa aquí" color="primary" class="q-mt-md" no-caps to="/login" />
-          </div>
-        </div>
-      </q-carousel-slide>
-<!--      <q-carousel-slide :name="3" class="column no-wrap flex-center" img-src="WhatsApp Image 2023-08-05 at 15.53.45 (2).jpeg" >-->
-<!--        <div class="text-center full-width" style="background: rgba(0,0,0,.3); color: white; padding: 10px">-->
-<!--          <div :class="`${$q.screen.lt.md ? 'text-h6' : 'text-h3'} text-left`" style="width: 650px;padding-left: 100px">-->
-<!--          </div>-->
-<!--          “FECHAS 13 - 14 - 15 - 16 y 17 DE SEPTIEMBRE DE 2023”-->
-<!--          <div>-->
-<!--            <q-btn label="Ingresa aquí" color="primary" class="q-mt-md" no-caps to="/login" />-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </q-carousel-slide>-->
       <template v-slot:control>
         <q-carousel-control
           position="top-right"
@@ -57,126 +33,38 @@
           class="text-white rounded-borders"
         >
           <div>
-            <q-toolbar class="text-primary">
-      <q-btn flat label="Principal" to="/principal"></q-btn>
-      <q-btn flat label="Principal" ></q-btn>
-      <q-toolbar-title>
-        <q-btn label="Ingresar" color="primary" to="/login" no-caps size="14px" icon="o_person" dense />
-      </q-toolbar-title>
-      <q-btn flat round dense icon="more_vert" />
-    </q-toolbar>
+            <q-btn icon="o_menu" v-if="$q.screen.lt.md" flat @click="modalMenu= true"/>
+            <q-toolbar class="" v-else>
+              <q-btn flat label="Principal" to="/principal" no-caps></q-btn>
+              <q-btn flat label="Quienes Somos" no-caps></q-btn>
+              <q-btn flat label="Objetivos" no-caps></q-btn>
+              <q-btn flat label="Ponentes" no-caps></q-btn>
+              <q-btn label="Ingresar" color="primary" to="/login" no-caps size="14px" />
+            </q-toolbar>
           </div>
-<!--          <q-toggle dense dark color="orange" v-model="autoplay" label="Auto Play" />-->
         </q-carousel-control>
       </template>
     </q-carousel>
-<!--    <q-header elevated>-->
-<!--      <div class="bg-teal-9">-->
-<!--        <q-btn flat dense round size="10px" icon="fa-brands fa-facebook" color="white" />-->
-<!--        <q-btn flat dense round size="10px" icon="fa-brands fa-youtube" color="white" />-->
-<!--      </div>-->
-<!--      <q-toolbar>-->
-<!--&lt;!&ndash;        <q-btn&ndash;&gt;-->
-<!--&lt;!&ndash;          flat&ndash;&gt;-->
-<!--&lt;!&ndash;          dense&ndash;&gt;-->
-<!--&lt;!&ndash;          round&ndash;&gt;-->
-<!--&lt;!&ndash;          icon="menu"&ndash;&gt;-->
-<!--&lt;!&ndash;          aria-label="Menu"&ndash;&gt;-->
-<!--&lt;!&ndash;          @click="toggleLeftDrawer"&ndash;&gt;-->
-<!--&lt;!&ndash;        />&ndash;&gt;-->
-
-<!--        <q-toolbar-title>-->
-<!--          <div class="text-subtitle2" style="line-height: 1em">-->
-<!--            4ta Jornada Nacional de-->
-<!--            Neurología-->
-<!--            <br>-->
-<!--            2do Simposio Internacional de-->
-<!--            Neurología-->
-<!--          </div>-->
-<!--        </q-toolbar-title>-->
-
-<!--        <div>-->
-<!--          <q-btn label="Ingresar" color="red" to="/login" no-caps size="14px" icon="login" />-->
-<!--        </div>-->
-<!--      </q-toolbar>-->
-<!--    </q-header>-->
-
-<!--    <q-drawer-->
-<!--      v-model="leftDrawerOpen"-->
-<!--      show-if-above-->
-<!--      bordered-->
-<!--    >-->
-<!--      <q-list>-->
-<!--        <q-item-label-->
-<!--          header-->
-<!--        >-->
-<!--          Essential Links-->
-<!--        </q-item-label>-->
-
-<!--        <EssentialLink-->
-<!--          v-for="link in essentialLinks"-->
-<!--          :key="link.title"-->
-<!--          v-bind="link"-->
-<!--        />-->
-<!--      </q-list>-->
-<!--    </q-drawer>-->
 
     <q-page-container>
       <router-view />
     </q-page-container>
     <FooterComponent></FooterComponent>
+    <q-dialog v-model="modalMenu" position="top">
+      <q-card class="q-pa-md">
+        <q-card-section class="q-pa-md">
+          <div class="text-subtitle1 text-center">Principal</div>
+          <div class="text-subtitle1 text-center">Quienes Somos</div>
+          <div class="text-subtitle1 text-center">Objetivos</div>
+          <div class="text-subtitle1 text-center">Ponentes</div>
+          <div class="text-subtitle1 text-center"><q-btn label="Ingresar" color="primary" to="/login" no-caps size="14px" /></div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-layout>
 </template>
 
 <script>
-// import { defineComponent, ref } from 'vue'
-// import EssentialLink from 'components/EssentialLink.vue'
-//
-// const linksList = [
-//   {
-//     title: 'Docs',
-//     caption: 'quasar.dev',
-//     icon: 'school',
-//     link: 'https://quasar.dev'
-//   },
-//   {
-//     title: 'Github',
-//     caption: 'github.com/quasarframework',
-//     icon: 'code',
-//     link: 'https://github.com/quasarframework'
-//   },
-//   {
-//     title: 'Discord Chat Channel',
-//     caption: 'chat.quasar.dev',
-//     icon: 'chat',
-//     link: 'https://chat.quasar.dev'
-//   },
-//   {
-//     title: 'Forum',
-//     caption: 'forum.quasar.dev',
-//     icon: 'record_voice_over',
-//     link: 'https://forum.quasar.dev'
-//   },
-//   {
-//     title: 'Twitter',
-//     caption: '@quasarframework',
-//     icon: 'rss_feed',
-//     link: 'https://twitter.quasar.dev'
-//   },
-//   {
-//     title: 'Facebook',
-//     caption: '@QuasarFramework',
-//     icon: 'public',
-//     link: 'https://facebook.quasar.dev'
-//   },
-//   {
-//     title: 'Quasar Awesome',
-//     caption: 'Community Quasar projects',
-//     icon: 'favorite',
-//     link: 'https://awesome.quasar.dev'
-//   }
-// ]
-
 import FooterComponent from 'components/FooterComponent.vue'
 
 export default {
@@ -184,10 +72,53 @@ export default {
   components: { FooterComponent },
   data () {
     return {
-      slide: 1
-      // leftDrawerOpen: ref(false),
-      // essentialLinks: linksList
+      modalMenu: false,
+      slide: 1,
+      textos: [
+        {
+          titulo: '4ta. JORNADA NACIONAL DE NEUROLOGÍA',
+          subtitulo: 'Actualización en Enfermedades Neurológicas',
+          fecha: 'Fechas: 13, 14, 15 de septiembre',
+          img: 'WhatsApp Image 2023-08-05 at 15.53.45.jpeg'
+        },
+        {
+          titulo: '2DO. SIMPOSIO INTERNACIONAL',
+          subtitulo: 'Emergencias Neurológicas',
+          fecha: 'Fechas: 16 y 12 de septiembre',
+          img: 'WhatsApp Image 2023-08-05 at 15.53.45 (1).jpeg'
+        }
+      ]
     }
   }
 }
 </script>
+<style>
+.background {
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  width: 100%;
+  height: 100%;
+}
+.titulo{
+  padding: 50px 80px;
+  font-size: 50px;
+  font-weight: 900;
+}
+.subtitulo{
+  padding: 50px 80px;
+  font-size: 30px;
+  font-weight: 900;
+}
+@media (max-width: 1024px) {
+  .titulo{
+    padding: 50px 0px 0px 0px;
+    font-size: 25px;
+    font-weight: 900;
+  }
+  .subtitulo{
+    padding: 5px 10px;
+    font-size: 15px;
+    font-weight: 900;
+  }
+}
+</style>
