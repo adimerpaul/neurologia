e7cb83754d507b724b65d9f6a728d60d1c1a247e
adimerpaul <adimer101@gmail.com>
2023-08-29 07:25:12 -0400

    actualizado ultimo version de carrocel

--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -2,93 +2,122 @@
   <q-page>
     <div class="bannerMenu">
       <div class="q-pa-md text-white text-h4 text-bold">
-        <q-img src="images/quir blanco2.png" width="150px" />
-        <span class="text-center textMenu">
-<!--          4ta. JORNADA NACIONAL DE NEUROLOGÍA y 2DO. SIMPOSIO INTERNACIONAL-->
-        </span>
+        <div class="row">
+          <div class="col-12 col-md-1 text-center">
+            <q-img src="images/quir blanco2.png" width="70px" />
+          </div>
+          <div class="col-12 col-md-2 flex flex-center">
+            <span class="text-center textMenu changa">
+              Neuro Oruro
+            </span>
+          </div>
+          <div class="col-12 col-md-7"></div>
+          <div class="col-12 col-md-2 flex flex-center">
+            <div class="text-right">
+              <q-input
+                outlined
+                dense
+                placeholder="Buscar"
+                bg-color="white"
+                border-color="white"
+                clearable
+              >
+                <template v-slot:append>
+<!--                  <div class="bg-blue">-->
+                    <q-icon name="search" />
+<!--                  </div>-->
+                </template>
+              </q-input>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <q-carousel
-      :height="$q.screen.lt.md ? '35vh' : '50vh'"
+      :height="$q.screen.lt.md ? '35vh' : '60vh'"
       swipeable
       animated
       v-model="slide"
       arrows
       navigation-icon="radio_button_unchecked"
+      class="q-mt-xs"
       infinite
       autoplay
       dark
     >
-      <q-carousel-slide v-for="(t, i) in textos" :name="i+1" :key="i" class="column no-wrap flex-center q-pa-none" :img-src="t.img">
+      <q-carousel-slide v-for="(t, i) in textos" :name="i+1" :key="i" class="column no-wrap flex-center" :img-src="t.img">
         <div class="text-center full-width q-pa-lg background">
-          <div class="text-left titulo">
-            {{ t.titulo }}
+          <div class="flex flex-center">
+            <div class="titulo changa">
+              {{ t.titulo }}
+            </div>
           </div>
-          <div class="subtitulo">
+          <div class="subtitulo changa">
             {{ t.subtitulo }}
+            <br>
+            {{ t.fecha }}
           </div>
-          <br>
-          {{ t.fecha }}
           <div>
-            <!--            <q-btn label="Ingresa aquí" color="primary" class="q-mt-md" no-caps to="/login" />-->
+            <q-btn color="primary" class="q-mt-md " no-caps to="/login">
+              <div class="changa button" >Ingresa Aqui</div>
+            </q-btn>
           </div>
+          <div style="position: absolute; bottom: 0; right: 0; padding: 10px;">
+            <div class="flex flex-center">
+                <q-img src="images/logoColMed3.png" width="50px" />
+                <div class="changa tituloCarrocel">
+                  Colegio Médico <br>
+                  De Oruro
+                </div>
+            </div>
+          </div>
+<!--          <q-img src="images/logoColMed3.png" width="30px" />-->
+<!--          <div class="changa tituloCarrocel">-->
+<!--            Colegio Medico Oruro-->
+<!--          </div>-->
         </div>
       </q-carousel-slide>
+<!--      <template v-slot:control>-->
+<!--        <q-carousel-control-->
+<!--          position="bottom-right"-->
+<!--          :offset="[18, 18]"-->
+<!--          class="text-white rounded-borders"-->
+<!--          style="background: rgba(0, 0, 0, .3); padding: 4px 8px;"-->
+<!--        >-->
+<!--          <q-img src="images/logoColMed3.png" width="30px" />-->
+<!--          <div class="changa tituloCarrocel">-->
+<!--            Colegio Medico Oruro-->
+<!--          </div>-->
+<!--        </q-carousel-control>-->
+<!--      </template>-->
     </q-carousel>
     <div>
-      <div class="text-h2 text-center text-bold">Bienvenidos!!</div>
       <div class="text-center">
-        <q-img src="images/porta2023.jpg" width="700px" />
+        <span class="changa text-h6 text-bold text-white bg-grey-7 q-pa-xs borderRadius">
+          AUSPICIADORES
+        </span>
       </div>
     </div>
     <div class="row">
-<!--      <div class="col-2 col-md-1"></div>-->
-<!--      <div class="col-8 col-md-4">-->
-<!--        <div class="text-subtitle2 text-bold text-center">-->
-<!--          4ta. JORNADA NACIONAL DE NEUROLOGÍA-->
-<!--        </div>-->
-<!--        <div class="text-subtitle2 text-bold text-center">-->
-<!--          Actualización en Enfermedades Neurológicas <br>-->
-<!--          Fechas: 13, 14, 15 de septiembre-->
-<!--        </div>-->
-<!--        <q-img src="356879877_584668113818175_8097994551976256420_n.jpg" />-->
-<!--      </div>-->
-<!--      <div class="col-2 col-md-1"></div>-->
-
-<!--      <div class="col-2 col-md-1"></div>-->
-<!--      <div class="col-8 col-md-4">-->
-<!--        <div class="text-subtitle2 text-bold text-center">-->
-<!--          2DO. SIMPOSIO INTERNACIONAL <br>-->
-<!--        </div>-->
-<!--        <div class="text-subtitle2 text-bold text-center">-->
-<!--          Emergencias Neurológicas <br>-->
-<!--          Fechas: 16 y 12 de septiembre-->
-<!--        </div>-->
-<!--        <q-img src="363427687_603117188639934_2648311829907974764_n.jpg" />-->
-<!--      </div>-->
-      <div class="col-2 col-md-1"></div>
-      <div class="col-12">
-        <div class="text-subtitle2 text-bold text-center">
-          Auspiciado por:
-        </div>
-      </div>
-      <div class="col-2 flex flex-center">
-        <q-img src="logos/bago.png" width="80px" />
-      </div>
-      <div class="col-2 flex flex-center">
-        <q-img src="logos/breskot pharma.png" width="80px" />
-      </div>
-      <div class="col-2 flex flex-center">
-        <q-img src="logos/ferrer.png" width="80px" />
-      </div>
-      <div class="col-2 flex flex-center">
-        <q-img src="logos/IMG_20230805_161256.jpg" width="80px" />
-      </div>
-      <div class="col-2 flex flex-center">
-        <q-img src="logos/megalabs.png" width="80px" />
-      </div>
-      <div class="col-2 flex flex-center">
-        <q-img src="logos/saval.png" width="80px" />
+      <div class="col-12 flex flex-center bg-grey-2">
+          <div class="q-pa-md">
+            <q-img src="logos/bago.png" width="100px" />
+          </div>
+          <div class="q-pa-md">
+            <q-img src="logos/breskot pharma.png" width="100px" />
+          </div>
+          <div class="q-pa-md">
+            <q-img src="logos/ferrer.png" width="100px" />
+          </div>
+          <div class="q-pa-md">
+            <q-img src="logos/IMG_20230805_161256.jpg" width="100px" />
+          </div>
+          <div class="q-pa-md">
+            <q-img src="logos/megalabs.png" width="100px" />
+          </div>
+          <div class="q-pa-md">
+            <q-img src="logos/saval.png" width="100px" />
+          </div>
       </div>
     </div>
   </q-page>
@@ -107,15 +136,15 @@ export default defineComponent({
         {
           titulo: '4ta. JORNADA NACIONAL DE NEUROLOGÍA',
           subtitulo: 'Actualización en Enfermedades Neurológicas',
-          fecha: 'Fechas: 13, 14, 15 de septiembre',
+          fecha: '13, 14, 15 de septiembre',
           img: 'WhatsApp Image 2023-08-05 at 15.53.45.jpeg'
-        },
-        {
-          titulo: '2DO. SIMPOSIO INTERNACIONAL',
-          subtitulo: 'Emergencias Neurológicas',
-          fecha: 'Fechas: 16 y 12 de septiembre',
-          img: 'WhatsApp Image 2023-08-05 at 15.53.45 (1).jpeg'
         }
+        // {
+        //   titulo: '2DO. SIMPOSIO INTERNACIONAL',
+        //   subtitulo: 'Emergencias Neurológicas',
+        //   fecha: '16 y 12 de septiembre',
+        //   img: 'WhatsApp Image 2023-08-05 at 15.53.45 (1).jpeg'
+        // }
       ]
     }
   }
@@ -123,24 +152,40 @@ export default defineComponent({
 </script>
 <style>
 .background {
-  background-color: rgba(0, 0, 0, 0.4);
+  background-color: rgba(0, 0, 0, 0.5);
   color: white;
-  width: 100%;
-  height: 100%;
 }
 .titulo{
   padding: 10px;
   font-size: 50px;
-  font-weight: 900;
+  line-height: 1;
+  letter-spacing: 1px;
+  max-width: 600px;
+  //font-weight: 900;
+}
+.button{
+  max-width: 50px;
+  line-height: 1
 }
 .subtitulo{
-  padding: 50px 80px;
+  padding: 10px 30px;
   font-size: 30px;
-  font-weight: 900;
+  //font-weight: 900;
+}
+.borderRadius{
+  border-radius: 10px;
 }
 .bannerMenu{
-  background: url('images/WhatsApp Image 2023-08-28 at 06.49.19.jpeg') no-repeat center center;
+  background: url('images/portadaTono2.jpg') no-repeat center center;
   background-size: cover;
+}
+.tituloCarrocel{
+  padding: 10px;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: 1px;
+  max-width: 180px;
+  //font-weight: 900;
 }
 @media (max-width: 1024px) {
   .titulo{
