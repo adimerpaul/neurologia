8a0ad150a480bed15492c29c4331b4035754cb1c
adimerpaul <adimer101@gmail.com>
2025-05-23 05:02:46 -0400

    Actulizado para registro

--- back/app/Models/Registro.php
+++ back/app/Models/Registro.php
@@ -26,7 +26,7 @@ class Registro extends Model{
         'file2'
     ];
     protected $hidden = [
-        'created_at',
+//        'created_at',
         'updated_at',
         'deleted_at'
     ];
