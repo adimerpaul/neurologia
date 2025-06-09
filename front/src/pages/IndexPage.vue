833c019f0f67dae1cb51fcb4f89ad039d30504af
adimerpaul <adimer101@gmail.com>
2025-06-09 06:03:34 -0400

    Actulizado

--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -64,7 +64,7 @@
               <div class="changa button">Inscribirse</div>
             </q-btn>
           </div>
-          <div style="position: absolute; bottom: 0; right: 0; padding: 10px 10px 10px 10px;margin: 5px;width: 200px;height:50px;">
+          <div style="position: absolute; top: 0; right: 0; padding: 0;margin: 5px;width: 150px;line-height: 0.9;">
             <div style="display: flex; align-items: center; justify-content: center;">
               <div class="changa" v-html="t.textButton">
               </div>
